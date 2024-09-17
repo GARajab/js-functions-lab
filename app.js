c1c7fa2c7ad2ca1983677425af79f784d6e787de
GARajab <mohamed.rajab@ewa.bh>
2024-09-17 21:03:41 +0300

    7 solved

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
 const maxOfTwoNumbers = (x, y) => {
   if (x >= y) {
     return x
@@ -28,3 +38,109 @@ const isAdult = (enteredAge) => {
 }
 
 console.log("Exercise 2 Result:", isAdult(21))
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+// hint: vowel =  a, e, i, o, u
+
+const isCharAVowel = (theCharacter) => {
+  if (
+    theCharacter.toLowerCase() === "a" ||
+    theCharacter.toLowerCase() === "e" ||
+    theCharacter.toLowerCase() === "i" ||
+    theCharacter.toLowerCase() === "o" ||
+    theCharacter.toLowerCase() === "u"
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log("Exercise 3 Result:", isCharAVowel("C"))
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (theName, theDomain) => {
+  return `${theName}@${theDomain}`
+}
+
+console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"))
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser = (theName, theTime) => {
+  return `Good ${theTime}, ${theName}`
+}
+
+console.log("Exercise 5 Result:", greetUser("Sam", "morning"))
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+const maxOfThree = (numOne, numTwo, numThree) => {
+  if (numOne > numTwo && numOne > numThree) {
+    return numOne
+  } else if (numTwo > numOne && numTwo > numThree) {
+    return numTwo
+  } else {
+    return numThree
+  }
+}
+
+console.log("Exercise 6 Result:", maxOfThree(5, 10, 8))
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+const calculateTip = (bill, tip) => {
+  return (tip / 100) * bill
+}
+console.log("Exercise 7 Result:", calculateTip(50, 20))
